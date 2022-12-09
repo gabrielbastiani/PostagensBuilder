27ca7a123e2df68489c1a38f2a95fa45ebbf650c
Gabriel Campos de Bastiani <gabriel.bastiani@sumig.local>
2022-12-09 11:38:06 -0300

    'ajuste'

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -14,6 +14,7 @@ import { Text, ActivityIndicator } from 'react-native';
 import { api } from '../../services/api';
 import { Auth } from '../../contexts/Auth';
 
+
 function Login() {
 
   const navigation = useNavigation()
